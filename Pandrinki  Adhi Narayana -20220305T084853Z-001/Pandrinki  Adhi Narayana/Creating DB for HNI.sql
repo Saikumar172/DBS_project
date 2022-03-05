f5c8bd61e721e12c8ae4f809ea0bd1f5292f8f6b
Saikumar172 <69100857+Saikumar172@users.noreply.github.com>
2022-03-05 14:39:01 +0530

    narayana commit
    narayana commit

---
+++ Pandrinki  Adhi Narayana -20220305T084853Z-001/Pandrinki  Adhi Narayana/Creating DB for HNI.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Customer_Authennticate(Customer_id VARCHAR(75) , UserName VARCHAR(75), PASSWORD VARCHAR(35)) ; 
+
+
+
+CREATE TABLE Customer_Mapping (Customer_id VARCHAR(75)  , Customer_name VARCHAR(75)) ;     
+
+
+
+CREATE TABLE WM_Appointment(Customer_id VARCHAR(75)  , Shedule_Date DATE  ,  Shedule_time TIME , 
+
+STATUS VARCHAR(75) ) ;   
+
+
+
+
+
+
+
+
