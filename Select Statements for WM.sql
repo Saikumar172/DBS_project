da0c806cbbcf6a2720b03f441b959cc57b0a7975
Saikumar172 <69100857+Saikumar172@users.noreply.github.com>
2022-03-05 14:25:09 +0530

    Narayana commit
    commit

---
+++ Select Statements for WM.sql
@@ -0,0 +1,13 @@
+
+SELECT * FROM WM_Mapping ; 
+
+SELECT * FROM CM_Appointment ; 
+
+
+SELECT  w1.WM_name , w1.WM_id ,ap.Shedule_Date , ap.Shedule_time , ap.Status  
+
+FROM CM_Appointment ap INNER JOIN  
+
+WM_Mapping w1 ON  w1.WM_id = ap.WM_id 
+
+ORDER BY ap.Shedule_Date ;  
---
+++ Update from WM.sql
@@ -0,0 +1,27 @@
+// WM can updated the value 
+
+
+
+
+UPDATE cm_appointment 
+
+SET STATUS = '' 
+
+WHERE WM_id = ' '   ; 
+
+
+Accept - IN progress  
+
+Reject -  Rejected    
+
+
+
+UPDATE cm_appointment 
+
+SET Shedule_time = '' 
+
+WHERE WM_id = ' '   ; 
+
+ 
+
+
